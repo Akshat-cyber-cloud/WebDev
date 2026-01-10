d7da20fe4f3c226be220300da9b63c583cc541b1
Akshat Gupta <149595957+Akshat-cyber-cloud@users.noreply.github.com>
2026-01-10 16:02:48 +0530

    Routing

--- React/Routing1/src/App.jsx
+++ React/Routing1/src/App.jsx
@@ -1,11 +1,62 @@
 import React from 'react'
+import { createBrowserRouter, Link, NavLink, Route, RouterProvider, Routes } from 'react-router-dom'
+import Home from './Components/Home'
+import Contact from './Components/Contact'
+import Courses from './Components/Courses'
+import Kodex from './Components/Kodex'
+import Kodr from './Components/Kodr'
+import AllCourses from './Components/AllCourses'
+import Footer from './Components/Footer'
+import { create } from 'framer-motion/m'
 
 const App = () => {
+
+  // const router = createBrowserRouter(
+  //   [
+  //     {
+  //       path: '/',
+  //       element: <Home />
+  //     },
+  //     {
+  //       path: '/contact',
+  //       element: <Contact />
+  //     },
+  //     {
+  //       path: '/courses',
+  //       element: <Courses />
+  //     }
+  //   ]
+  // )
+
   return (
-    <div>
-      
+    <div className='main'>
+      <nav className='navbar'>
+        <NavLink style={
+          ({ isActive }) => { return { color: isActive ? 'lightblue' : 'black' } }
+        } to='/'>Home</NavLink> |
+        <NavLink to='/contact' style={({ isActive }) => { return { color: isActive ? 'red' : 'black' } }}>Contact</NavLink> |
+
+        <NavLink to='/courses'>Courses</NavLink> |
+      </nav>
+
+      <div className="content">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/courses' element={<Courses />} > 
+            <Route path='/courses' element={<AllCourses />} />
+            <Route path = '/courses/kodr' element={<Kodr />} />
+            <Route path = '/courses/kodex' element={<Kodex />} />
+          </Route>
+        </Routes>
+      </div>
+
+      {/* <RouterProvider router={router} /> */}
+
+      <Footer />
     </div>
   )
 }
 
 export default App
+
---
+++ React/Routing1/src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Footer = () => {
+    const navigate = useNavigate();
+  return (
+    <div className='footer'>
+        <h2>Footer</h2>
+        <button onClick={() => navigate('/courses')}>Explore More Courses</button>
+    </div>
+  )
+}
+
+export default Footer
