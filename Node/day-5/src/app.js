25d8aad8953cd0ab204da3eaf72a67b45f7c0f8e
Akshat Gupta <149595957+Akshat-cyber-cloud@users.noreply.github.com>
2026-01-27 23:52:20 +0530

    day-5

---
+++ Node/day-5/src/app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const notes = [
+    {
+        title: "Hawk",
+        description: "Sharp Eyes" 
+    }
+];
+
+app.post('/notes', (req, res) => {
+    notes.push(req.body);   
+
+    res.status(201).json({
+        message: "Note Created Sucessfully"
+    })
+});
+
+app.get('/notes',(req,res) => {
+    res.status(200).json({
+        message: "Sucessfully Fetched",
+        notes:notes
+    })
+})
+
+app.delete('/notes/:index', (req,res) => {
+    delete notes[req.params.index];
+
+    res.status(204).json({
+        message: "Note Deleted Successfully"
+    })
+})
+
+app.patch("/notes/:index", (req,res) => {
+    notes[req.params.index].description = req.body.description;
+
+    res.status(200).json({
+        message: "Successfully Updated"
+    })
+})
+
+
+
+
+module.exports = app;
