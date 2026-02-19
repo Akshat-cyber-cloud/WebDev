ede812228ad876d072d1e8650abd7621b576feea
Akshat Gupta <149595957+Akshat-cyber-cloud@users.noreply.github.com>
2026-02-19 23:46:37 +0530

    Track3

--- Node/day-12P/src/routes/post.route.js
+++ Node/day-12P/src/routes/post.route.js
@@ -7,6 +7,10 @@ const upload = multer({storage: multer.memoryStorage()});
 
 postRouter.post("/", upload.single("image"), postController.createPostController);
 
+postRouter.get("/", postController.getPostsController);
+
+postRouter.get("/details/:postId", postController.getPostDetailsController);
+
 
 
 module.exports = postRouter;
