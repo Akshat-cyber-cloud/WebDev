07cb79aa1d6266a8c716dd139de7ef5c5d6c144f
Akshat Gupta <149595957+Akshat-cyber-cloud@users.noreply.github.com>
2026-03-01 18:57:49 +0530

    Moodify Day3

--- Project1/Backend/src/controllers/auth.controller.js
+++ Project1/Backend/src/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 const userModel = require('../models/user.model');
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken');
-const blacklistModel = require('../models/blacklist.model')
+const blacklistModel = require('../models/blacklist.model');
+const redis = require('../config/cache');
 
 async function registerUser(req,res){
     const {username,email,password} = req.body;
@@ -90,9 +91,10 @@ async function logoutUser(req,res){
     const token = req.cookies.token;
     res.clearCookie("token");
 
-    await blacklistModel.create({
-        token
-    })
+    // await blacklistModel.create({
+    //     token
+    // })
+    redis.set(token,Date.now().toString(),"EX", 60 * 60);
 
     res.status(200).json({
         message: "Logged Out Successfully"
