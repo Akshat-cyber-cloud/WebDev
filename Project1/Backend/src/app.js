29dfb90e685c72756f15d5a8ced75fb1a5ceec65
Akshat Gupta <149595957+Akshat-cyber-cloud@users.noreply.github.com>
2026-03-04 15:45:56 +0530

    Backend C

--- Project1/Backend/src/app.js
+++ Project1/Backend/src/app.js
@@ -12,7 +12,10 @@ app.use(cookieParser());
 
 
 const authRoutes = require('./routes/auth.routes');
+const songRoutes = require('./routes/song.routes');
+
 app.use("/api/auth", authRoutes);
+app.use("/api/songs", songRoutes);
 
 
 
