f438b2508b3c6e2c712f9d5d92d67f7ab3c335ac
Akshat Gupta <149595957+Akshat-cyber-cloud@users.noreply.github.com>
2026-03-17 17:53:02 +0530

    Track

--- Node/Gen-AI/index.js
+++ Node/Gen-AI/index.js
@@ -1,7 +1,22 @@
 import "dotenv/config"
 import readline from "readline/promises";
 import { ChatMistralAI } from "@langchain/mistralai";
-import {HumanMessage} from "langchain";
+import {HumanMessage, tool, createAgent} from "langchain";
+import { sendEmail } from "./mail.service.js";
+import * as z from "zod";
+
+const emailTool = tool(
+    sendEmail,
+    {
+        name: "mailTool",
+        desc: "Use this tool to send mails",
+        schema: z.object({
+            to: z.string().describe("The recipient's email address"),
+            html: z.string().describe("The HTML content of the email"),
+            subject: z.string().describe("The subject of the email"),
+        })
+    }
+)
 
 const r1 = readline.createInterface({
     input: process.stdin,
@@ -17,16 +32,23 @@ const model = new ChatMistralAI({
     model: "mistral-small-latest",
 })
 
+const agent = createAgent({
+    model,
+    tools: [emailTool]
+})
+
 const messages = [];
 
 while(true){
     const userInput = await r1.question("\x1b[32mYou:\x1b[0m ")
     messages.push(new HumanMessage(userInput));
 
-    const response = await model.invoke(messages);
-    messages.push(response);
+    const response = await agent.invoke({
+        messages
+    });
+    messages.push(response.messages[response.messages.length - 1]);
 
-    console.log("AI: " + response.text);
+     console.log(`\x1b[34m[AI]\x1b[0m ${response.messages[ response.messages.length - 1 ].content}`)
 }
 
 
