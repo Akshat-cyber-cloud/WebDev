2782aaa51af782f33abf29dfd20a8cd0b6ff9ae9
Akshat Gupta <149595957+Akshat-cyber-cloud@users.noreply.github.com>
2026-04-04 23:21:05 +0530

    Socket

--- Node/day-15/server.js
+++ Node/day-15/server.js
@@ -1,21 +1,21 @@
-// import app from "./src/app.js"
-// import { createServer } from "http";
-// import { Server } from "socket.io";
+import app from "./src/app.js"
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-// const httpServer = createServer(app);
-// const io = new Server(httpServer, { /* options */ });
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
 
-// io.on("connection", (socket) => {
-//     console.log("New Connection Created");
+io.on("connection", (socket) => {
+    console.log("New Connection Created");
 
-//     socket.on("message", (msg) => {
-//         console.log("User fired message event");
-//         console.log(msg);
-//         io.emit("abc",msg);
-//     })
-// });
-// httpServer.listen(3000, () => {
-//     console.log("Server is running on port 3000");
-// });
+    socket.on("message", (msg) => {
+        console.log("User fired message event");
+        console.log(msg);
+        io.emit("abc",msg);
+    })
+});
+httpServer.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
 
 
