34afd7da098388b3d0b2ce0ead83f16d4b6f05da
Akshat Gupta <149595957+Akshat-cyber-cloud@users.noreply.github.com>
2026-04-19 19:12:28 +0530

    Deployment Status 1

--- Project2/Backend/src/utils/generateToken.ts
+++ Project2/Backend/src/utils/generateToken.ts
@@ -3,8 +3,8 @@ import jwt from 'jsonwebtoken';
 
 const generateToken = (res: Response, userId: string) => {
   const token = jwt.sign(
-    { userId }, 
-    (process.env.JWT_SECRET as string) || 'secret', 
+    { userId },
+    (process.env.JWT_SECRET as string) || 'secret',
     {
       expiresIn: (process.env.JWT_EXPIRE as any) || '30d',
     }
@@ -15,7 +15,7 @@ const generateToken = (res: Response, userId: string) => {
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'development', // Use secure in production
-    sameSite: 'strict', // Prevent CSRF attacks
+    sameSite: process.env.NODE_ENV === 'development' ? 'strict' : 'none', 
     maxAge: cookieExpireDays * 24 * 60 * 60 * 1000,
   });
 };
